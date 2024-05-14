d3e5d79811a435d077361a4c82e658d0c43fb486
Ena Jovanovic <ena@fundapps.co>
2024-05-14 13:19:53 +0100

    removed game over button

--- disclose.ai.ui/src/app/ControlCentre.tsx
+++ disclose.ai.ui/src/app/ControlCentre.tsx
@@ -12,7 +12,6 @@ const ControlCentre = () => {
             <button className="btn btn-primary" onClick={() => navigate('/splash')}>Splash</button>
             <button className="btn btn-secondary" onClick={() => navigate('/main')}>Main</button>
             <button className="btn btn-accent" onClick={() => navigate('/rank')}>Rank</button>
-            <button className="btn btn-quadernary" onClick={() => navigate('/game-over')}>Game Over</button>
         </div>
     );
 }
