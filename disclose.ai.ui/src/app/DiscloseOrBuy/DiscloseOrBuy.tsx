228d65bde011c332e7a9eb3f5064c12c60cc9a4c
Anibe Agamah <anibe@fundapps.co>
2024-05-14 15:20:45 +0100

    Display different screen based on game state

--- disclose.ai.ui/src/app/DiscloseOrBuy/DiscloseOrBuy.tsx
+++ disclose.ai.ui/src/app/DiscloseOrBuy/DiscloseOrBuy.tsx
@@ -2,6 +2,8 @@ import { useContext, useState } from 'react';
 import Header from '../../shared/header';
 import InvestmentSlider from './components/InvestmentSlider';
 import { GameStateContext } from '../RouterStateManager';
+import { GameStates } from '../types';
+import Rank from '../Rank/Rank';
 
 const DiscloseOrBuy = () => {
   const [hasReadRules, setHasReadRules] = useState(false);
@@ -10,7 +12,7 @@ const DiscloseOrBuy = () => {
     <div>
       <Header />
 
-      {!hasReadRules && (
+      {!hasReadRules && gameState.state === GameStates.waiting && (
         <div className="card w-96 bg-base-100 shadow-xl mx-auto mb-4">
           <div className="card-body">
             <h2 className="card-title">Regulator Rule</h2>
@@ -37,22 +39,44 @@ const DiscloseOrBuy = () => {
         </div>
       )}
 
-      <div className="card w-96 bg-base-100 shadow-xl mx-auto mb-4">
-        <div className="card-body">
-          <h2 className="card-title">Your portfolio</h2>
-          <button className="btn btn-secondary" onClick={() => console.log('disclose')}>
-              Disclose?
-            </button>
-          {gameState.turn.companies.map((company) => (
-            <InvestmentSlider key={company} name={company} maxAmount={gameState.turn.budget} />
-          ))}
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary" onClick={() => console.log('buy')}>
-              Buy
-            </button>
+      {gameState.state === GameStates.waiting && (
+        <div className="card w-96 bg-base-100 shadow-xl mx-auto mb-4">
+          <div className="card-body">
+            <h2 className="card-title">Loading game...</h2>
+            <div>Waiting for other players to join</div>
           </div>
         </div>
-      </div>
+      )}
+
+      {gameState.state === GameStates.turn && (
+        <div className="card w-96 bg-base-100 shadow-xl mx-auto mb-4">
+          <div className="card-body">
+            <h2 className="card-title">Your portfolio</h2>
+            <button className="btn btn-secondary" onClick={() => console.log('disclose')}>
+              Disclose?
+            </button>
+            {gameState.turn.companies.map((company) => (
+              <InvestmentSlider key={company} name={company} maxAmount={gameState.turn.budget} />
+            ))}
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary" onClick={() => console.log('buy')}>
+                Buy
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {gameState.state === GameStates.endOfTurn && (
+        <div className="card w-96 bg-base-100 shadow-xl mx-auto mb-4">
+          <div className="card-body">
+            <h2 className="card-title">Your investment has been selected</h2>
+            <div>Results will be ready soon</div>
+          </div>
+        </div>
+      )}
+
+      {gameState.state === GameStates.end && <Rank />}
     </div>
   );
 };
