212ce9aad86c10bbbca1d460654a490b2d04ea7c
Vincent Gilbert <51780086+Vincent-FundApps@users.noreply.github.com>
2024-05-14 13:44:01 +0100

    Add Rank page

--- disclose.ai.ui/src/app/Rank/Rank.tsx
+++ disclose.ai.ui/src/app/Rank/Rank.tsx
@@ -1,3 +1,20 @@
-const Rank = () => <div />;
+import { useContext } from "react";
+import { GameStateContext } from "../RouterStateManager";
+
+const Rank = () => {
+    const { userPlayer } = useContext(GameStateContext);
+
+    return (
+    <div>
+      <h1>You think you're good?</h1>
+      <span className="badge-primary">{userPlayer.name}</span>
+    </div>
+    )
+};
+
+return(
+
+
+)
 
 export default Rank;
