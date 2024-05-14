e84bdb1c50209c640fab30b21f3152c2c049fb5d
Roberto Minghi <robertominghi@msn.com>
2024-05-14 11:58:35 +0100

    router on manager component

--- disclose.ai.ui/src/main.tsx
+++ disclose.ai.ui/src/main.tsx
@@ -1,36 +1,12 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import App from './app/app';
-import LoginPage from './app/LoginPage/LoginPage';
-import Rank from './app/Rank/Rank';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '*',
-    element: <div>Not Found</div>,
-  },
-  {
-    path: '/login',
-    element: <LoginPage />,
-  },
-  {
-    path: '/rank',
-    element: <Rank />,
-  },
-  { path: '/game-over', element: <div>Game Over</div> },
-]);
+import RouterManager from './app/RouterManager';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <RouterManager />
   </StrictMode>
 );
