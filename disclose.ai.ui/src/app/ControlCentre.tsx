eed45146ec92b90a2366aaf055534a33c00cdfdd
Vincent Gilbert <51780086+Vincent-FundApps@users.noreply.github.com>
2024-05-14 11:29:45 +0100

    Added Daisy Plugin

--- disclose.ai.ui/src/app/ControlCentre.tsx
+++ disclose.ai.ui/src/app/ControlCentre.tsx
@@ -1,8 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
+
 const ControlCentre = () => {
+    const navigate = useNavigate();
+
     return (
         <div>
             <h1 className="card">Control Centre</h1>
-            <button className="btn btn-primary" onClick={() => console.log('navigate to start game')}>Splash</button>
+            <button className="btn btn-primary" onClick={() => navigate('/splash')}>Splash</button>
             <button className="btn btn-secondary" onClick={() => console.log('navigate to reset game')}>Main</button>
             <button className="btn btn-accent" onClick={() => console.log('navigate to ranking game')}>Rank</button>
             <button className="btn btn-quadernary" onClick={() => console.log('navigate to ranking game')}>Game Over</button>
