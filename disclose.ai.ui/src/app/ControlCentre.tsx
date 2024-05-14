c455b4ccadc906a94a7374d895c1b82ae1e9657f
Vincent Gilbert <51780086+Vincent-FundApps@users.noreply.github.com>
2024-05-14 11:31:01 +0100

    Added routes

--- disclose.ai.ui/src/app/ControlCentre.tsx
+++ disclose.ai.ui/src/app/ControlCentre.tsx
@@ -8,9 +8,9 @@ const ControlCentre = () => {
         <div>
             <h1 className="card">Control Centre</h1>
             <button className="btn btn-primary" onClick={() => navigate('/splash')}>Splash</button>
-            <button className="btn btn-secondary" onClick={() => console.log('navigate to reset game')}>Main</button>
-            <button className="btn btn-accent" onClick={() => console.log('navigate to ranking game')}>Rank</button>
-            <button className="btn btn-quadernary" onClick={() => console.log('navigate to ranking game')}>Game Over</button>
+            <button className="btn btn-secondary" onClick={() => navigate('/main')}>Main</button>
+            <button className="btn btn-accent" onClick={() => navigate('/rank')}>Rank</button>
+            <button className="btn btn-quadernary" onClick={() => navigate('/game-over')}>Game Over</button>
 
         </div>
     );
