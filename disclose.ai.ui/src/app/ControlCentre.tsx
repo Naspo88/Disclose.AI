a9bf3661dcee27683031fe90d9c153e311dcef20
Anibe Agamah <anibe@fundapps.co>
2024-05-14 15:28:57 +0100

    Only show actions to admin

--- disclose.ai.ui/src/app/ControlCentre.tsx
+++ disclose.ai.ui/src/app/ControlCentre.tsx
@@ -1,19 +1,30 @@
-import { useNavigate } from "react-router-dom";
-import Header from "../shared/header";
-
+import { useNavigate } from 'react-router-dom';
+import Header from '../shared/header';
+import useStatePoller from './hooks/useStatePoller';
 
 const ControlCentre = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const { userPlayer } = useStatePoller();
 
-    return (
+  return (
+    <div>
+      <Header />
+      <h1 className="tex-xxl">Control Centre</h1>
+      {userPlayer.isAdmin && (
         <div>
-            <Header />
-            <h1 className="card">Control Centre</h1>
-            <button className="btn btn-primary" onClick={() => navigate('/splash')}>Splash</button>
-            <button className="btn btn-secondary" onClick={() => navigate('/main')}>Main</button>
-            <button className="btn btn-accent" onClick={() => navigate('/rank')}>Rank</button>
+          <button className="btn btn-primary" onClick={() => navigate('/splash')}>
+            Splash
+          </button>
+          <button className="btn btn-secondary" onClick={() => navigate('/main')}>
+            Main
+          </button>
+          <button className="btn btn-accent" onClick={() => navigate('/rank')}>
+            Rank
+          </button>
         </div>
-    );
-}
+      )}
+    </div>
+  );
+};
 
 export default ControlCentre;
