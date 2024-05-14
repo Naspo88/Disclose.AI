fe27acaecd7db3ae0e506c1955c36a86235eeeee
Vincent Gilbert <51780086+Vincent-FundApps@users.noreply.github.com>
2024-05-14 11:25:27 +0100

    Colour changes

--- disclose.ai.ui/src/app/ControlCentre.tsx
+++ disclose.ai.ui/src/app/ControlCentre.tsx
@@ -1,10 +1,12 @@
 const ControlCentre = () => {
     return (
         <div>
-            <h1>Control Centre</h1>
-            <button onClick={() => console.log('navigate to start game')}>Start game</button>
-            <button onClick={() => console.log('navigate to reset game')}>reset game</button>
-            <button onClick={() => console.log('navigate to ranking game')}>Ranking</button>
+            <h1 className="card">Control Centre</h1>
+            <button className="btn btn-primary" onClick={() => console.log('navigate to start game')}>Splash</button>
+            <button className="btn btn-secondary" onClick={() => console.log('navigate to reset game')}>Main</button>
+            <button className="btn btn-accent" onClick={() => console.log('navigate to ranking game')}>Rank</button>
+            <button className="btn btn-quadernary" onClick={() => console.log('navigate to ranking game')}>Game Over</button>
+
         </div>
     );
 }
